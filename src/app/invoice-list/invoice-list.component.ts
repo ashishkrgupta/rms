69abe49041ff5ec7ca578b8100fc434e35030ca5
Ashish Kumar Gupta <ashishkrgupta@hotmail.com>
2019-06-19 11:49:50 +0530

    implemented click handler method in component

--- src/app/invoice-list/invoice-list.component.ts
+++ src/app/invoice-list/invoice-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Invoice } from '../invoice';
 import { InvoiceService } from '../invoice.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-invoice-list',
@@ -12,10 +13,20 @@ export class InvoiceListComponent implements OnInit {
   invoices: Invoice[] = [];
   columnsToDisplay = ['id', 'customerName', 'contactNo', 'invoiceDate', 'total'];
 
-  constructor(private invoiceService: InvoiceService) { }
+  constructor(private invoiceService: InvoiceService, private router: Router) { }
 
   ngOnInit() {
     this.invoices = this.invoiceService.getInvoices();
+	//console.log(this.invoices);
+    // this.invoiceService.getInvoices().subscribe((invoices: Invoice[])=>{
+    //   this.invoices = invoices;
+    //   console.log(invoices);
+    // })
+  }
+  
+  invoiceDetail(row: any) {
+    console.log(row);
+    this.router.navigate(['/invoices/' + row.id]);
   }
 
 }
