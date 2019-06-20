ece80872b5810273245b14c32e877cd56ed1f34a
Ashish Kumar Gupta <ashishkrgupta@hotmail.com>
2019-06-20 16:49:45 +0530

    Update invoice-list.component.ts

--- src/app/invoice-list/invoice-list.component.ts
+++ src/app/invoice-list/invoice-list.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Invoice } from '../invoice';
 import { InvoiceService } from '../invoice.service';
-import {Router} from '@angular/router';
+import { Router } from '@angular/router';
+import { MatTableDataSource } from '../../../node_modules/@angular/material';
 
 @Component({
   selector: 'app-invoice-list',
@@ -10,20 +11,19 @@ import {Router} from '@angular/router';
 })
 export class InvoiceListComponent implements OnInit {
 
-  invoices: Invoice[] = [];
+  invoices: MatTableDataSource<Invoice> = new MatTableDataSource();
   columnsToDisplay = ['id', 'customerName', 'contactNo', 'invoiceDate', 'total'];
 
   constructor(private invoiceService: InvoiceService, private router: Router) { }
 
   ngOnInit() {
-    this.invoices = this.invoiceService.getInvoices();
-	//console.log(this.invoices);
-    // this.invoiceService.getInvoices().subscribe((invoices: Invoice[])=>{
-    //   this.invoices = invoices;
-    //   console.log(invoices);
-    // })
+    //this.invoices = this.invoiceService.getInvoices();
+    this.invoiceService.getInvoices().subscribe((invoices: Invoice[]) => {
+      this.invoices.data = invoices;
+      console.log(invoices);
+    })
   }
-  
+
   invoiceDetail(row: any) {
     console.log(row);
     this.router.navigate(['/invoices/' + row.id]);
